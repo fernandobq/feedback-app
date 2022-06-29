3e6710f5c3cbabd9c55fac3531c055730acd172c
Fernando Barraza <fernandobarraza@192.168.1.4>
2022-06-29 10:03:59 -0600

    form to add more feedback

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,29 @@
+import Card from "./shared/Card";
+import {useState} from "react";
+
+const FeedbackForm = () => {
+    const [text, setText] = useState('')
+
+    const handleTextChange = (event) => {
+        setText(event.target.value)
+    }
+    return (
+        <Card>
+            <form>
+                <h2>How would you rate your service with us?</h2>
+
+                <div className="input-group">
+                    <input
+                        type="text"
+                        placeholder="Write a review"
+                        onChange={handleTextChange}
+                        value={text}
+                    />
+                    <button type="submit">Send</button>
+                </div>
+            </form>
+        </Card>
+    )
+}
+
+export default FeedbackForm
